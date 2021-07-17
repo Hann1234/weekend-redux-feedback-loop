6bad56932970c591b6f2b411e2eca583fd226568
Hann1234 <christopherjhannigan@gmail.com>
2021-07-17 15:35:41 -0500

    added routes to App

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,16 +1,39 @@
 import React from 'react';
+import {useDispatch, useSelector} from 'react-redux';
+import { HashRouter as Router, Route, Link } from "react-router-dom";
 import axios from 'axios';
 import './App.css';
+import FeelingInput from '../FeelingInput/FeelingInput';
+import UnderstandingInput from '../UnderstandingInput/UnderstandingInput';
+import SupportInput from '../SupportInput/SupportInput';
 
 function App() {
 
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <h1 className='App-title'>Feedback!</h1>
-        <h4>Don't forget it!</h4>
-      </header>
-    </div>
+    <Router>
+      <div className='App'>
+        <header className='App-header'>
+          <h1 className='App-title'>Feedback!</h1>
+          <h4>Don't forget it!</h4>
+        </header>
+        <Route path='/' exact>
+          <FeelingInput />
+        </Route>
+        <Route path='/understanding' exact>
+          <UnderstandingInput />
+        </Route>
+        <Route path='/support' exact>
+          <SupportInput />
+        </Route> 
+        <Route path='/comments' exact>
+          <CommentsInput />
+        </Route> 
+        <Route path='/review' exact>
+          <Review />
+        </Route> 
+      </div>
+    </Router>
+
   );
 }
 
