2458d041d8ffdfa17b413222da30c0d47aac4ab1
Hann1234 <christopherjhannigan@gmail.com>
2021-07-17 14:49:48 -0500

    added reducers and store to index

--- src/index.js
+++ src/index.js
@@ -4,5 +4,49 @@ import './index.css';
 import App from './components/App/App';
 import registerServiceWorker from './registerServiceWorker';
 
+import {createStore, combineReducers, applyMiddleware} from 'redux';
+import logger from 'redux-logger';
+import {Provider} from 'react-redux';
+
+// Reducers:
+
+const feeling = (state=[], action) => {
+    if (action.type === 'ADD_FEELING'){
+        return action.payload;
+    }
+    return state;
+};
+
+const understanding = (state=[], action) => {
+    if (action.type === 'ADD_UNDERSTANDING'){
+        return action.payload;
+    }
+    return state;
+};
+
+const support = (state=[], action) => {
+    if (action.type === 'ADD_SUPPORT'){
+        return action.payload;
+    }
+    return state;
+};
+
+const comments = (state=[], action) => {
+    if (action.type === 'ADD_COMMENTS'){
+        return action.payload;
+    }
+    return state;
+};
+
+let storeInstance= createStore(
+    combineReducers({
+        feeling,
+        understanding,
+        support,
+        comments,
+    }),
+    applyMiddleware(logger)
+)
+
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
